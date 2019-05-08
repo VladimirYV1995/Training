e9d353f1d8f4f500665ff438cb58d29fe3c7b3cb
Vladimir Yurin <v.yurin.jr@mail.ru>
2019-05-08 22:48:23 +0300

    1st Tasks

---
+++ Tasks/dicts_in_list.py
@@ -0,0 +1,35 @@
+_from = {'a':[0,1], 'b':[2,3], 'c':[4,5]}
+
+_to=[]
+
+len_v=len(list(_from.values())[0])
+
+buf={}
+
+for i in range(len_v):
+    for key, val in _from.items():
+        buf[key]=list(val)[i]
+    _to.append(buf)
+
+print(_to)
+
+
+
+
+
+
+
+
+#print('keys:',_from.keys())
+#print('values:',_from.values())
+
+
+#print(key,':',val)
+
+
+
+#_to = [{'a':0,'b':2, 'c':4},{'a':1,'b':3, 'c':5}]
+
+
+
+

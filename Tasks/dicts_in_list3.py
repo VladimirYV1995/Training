b0630fedcdf3c3201b8f6c138dc656b8c0d94f59
Vladimir Yurin <v.yurin.jr@mail.ru>
2019-05-11 13:09:59 +0300

    Zip and map

--- Tasks/dicts_in_list3.py
+++ Tasks/dicts_in_list3.py
@@ -4,16 +4,16 @@ _from = {'a':[0,1], 'b':[2,3], 'c':[4,5]}
 
 _to =[] #_to = [{'a': 0, 'b': 2, 'c': 4}, {'a': 1, 'b': 3, 'c': 5}] 
 
-
 keys=_from.keys()
 vals=_from.values() 
+
 
 
 zip_val= list(zip(*vals))
 
 for zv in zip_val:
      buf=dict(map(lambda *arg: arg, keys,zv))
-    _to.append(buf)
+     _to.append(buf)
 
 print(_to)
 

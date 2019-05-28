7b8bcd6c2b48e06a4803ebfc48db4c6c0e6fbdb9
Vladimir Yurin <v.yurin.jr@mail.ru>
2019-05-28 19:50:03 +0300

    update_2

--- python/Zadachka.py
+++
@@ -1,5 +0,0 @@
-for key, val in _from.items():
-    print(key,':',val)
-
-print('keys:',_from.keys())
-print('values:',_from.values())
--- python/list.py
+++
@@ -1,3 +0,0 @@
-LIST_1=[1,2,3,4,5,6]
-LIST_1.append(77)
-print(LIST_1)

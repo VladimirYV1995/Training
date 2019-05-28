08973febd3d0d9b7e9dd5897fe653d517809996f
Vladimir Yurin <v.yurin.jr@mail.ru>
2019-05-28 19:53:24 +0300

    update_2

--- Github/python/Parser/ParserYandexNews.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
- 
-url = 'https://yandex.ru'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
- 
-for link in soup.find_all('a', class_='home-link list__item-content home-link_black_yes')[:5]:
-    print(link.text)

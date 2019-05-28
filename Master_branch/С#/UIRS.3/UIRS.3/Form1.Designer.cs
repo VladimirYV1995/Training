7ca35303ffad7cb93e55d17d3857c9efc5c9c20e
Vladimir Yurin <v.yurin.jr@mail.ru>
2019-05-28 19:56:38 +0300

    update_2

---
+++ Master_branch/С#/UIRS.3/UIRS.3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace UIRS._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.MakeDiagram = new System.Windows.Forms.Button();
+            this.Loadfile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(477, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Изменение цвета выбранного сектора";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView.Location = new System.Drawing.Point(477, 74);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(263, 352);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Цвет";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Категория ";
+            this.Column2.Name = "Column2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.panel);
+            this.groupBox1.Location = new System.Drawing.Point(158, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 358);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(20, 18);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(150, 17);
+            this.label.TabIndex = 4;
+            this.label.Text = "Подпись к диаграмме";
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Location = new System.Drawing.Point(6, 62);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(300, 290);
+            this.panel.TabIndex = 3;
+            // 
+            // MakeDiagram
+            // 
+            this.MakeDiagram.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.MakeDiagram.Location = new System.Drawing.Point(181, 12);
+            this.MakeDiagram.Name = "MakeDiagram";
+            this.MakeDiagram.Size = new System.Drawing.Size(247, 46);
+            this.MakeDiagram.TabIndex = 4;
+            this.MakeDiagram.Text = "Построить диаграмму ";
+            this.MakeDiagram.UseVisualStyleBackColor = true;
+            this.MakeDiagram.Click += new System.EventHandler(this.MakeDiagram_Click);
+            // 
+            // Loadfile
+            // 
+            this.Loadfile.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Loadfile.Location = new System.Drawing.Point(12, 12);
+            this.Loadfile.Name = "Loadfile";
+            this.Loadfile.Size = new System.Drawing.Size(140, 53);
+            this.Loadfile.TabIndex = 5;
+            this.Loadfile.Text = "Загрузить данные с XML файл";
+            this.Loadfile.UseVisualStyleBackColor = false;
+            this.Loadfile.Click += new System.EventHandler(this.Loadfile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 451);
+            this.Controls.Add(this.Loadfile);
+            this.Controls.Add(this.MakeDiagram);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Диаграмма";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button MakeDiagram;
+        private System.Windows.Forms.Button Loadfile;
+    }
+}
+

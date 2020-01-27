ad7281183ff2888c41f3116a3b8c2e8f3083fb81
Vladimir Yurin <v.yurin.jr@mail.ru>
2020-01-27 21:39:36 +0300

    C#Light_2

---
+++ С#/CSLightHomeWork/CSBasic/Variables.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForMyFuture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //основные типы
+
+            //целочисленные
+            byte bt;
+            sbyte sb;
+
+            short sh;
+            ushort ush;
+
+            int i;
+            uint ui;
+
+            long ln;
+            ulong uln;
+
+            // с плавующей точкой
+            float ft = 5.7f;
+            double db = 5.7;
+            //символьный тип 
+            char c = 'D';
+            //строковые 
+            string str = "Hi, There!";
+            //логическое
+            bool tr = true;
+            bool fle = false;
+
+
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}

d852822b81680618b54f12f7fac81bfd40649967
Vladimir Yurin <v.yurin.jr@mail.ru>
2020-02-25 17:31:50 +0300

    2D platformer v1.0

--- 2D platformer/Assets/Scripts/CollisionCoin.cs
+++ 2D platformer/Assets/Scripts/CollisionCoin.cs
@@ -7,13 +7,24 @@ public class CollisionCoin : MonoBehaviour
 {
     [SerializeField] private UnityEvent _pickUp;   
 
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<Coin>())
+        {
+            _pickUp?.Invoke();
+            collider.GetComponent<Coin>().Collect();            
+        }
+    }
+
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.GetComponent<Coin>())
         {
             _pickUp?.Invoke();
-            collision.collider.GetComponent<Coin>().Collect();            
+            collision.collider.GetComponent<Coin>().Collect();
         }
     }
-    
+    */
 }
+
